4533c96e10af926d2aabf23dc8ce922705bbb6ad
Aksiniia1983 <kelchaninova@gmail.com>
2022-04-21 12:12:01 -0400

    created a new file for questions

---
+++ src/QuestForNextDays.java
@@ -0,0 +1,16 @@
+public class QuestForNextDays {
+    /*
+    3.
+int a=5;
+int b=6;
+change and print
+a==6;
+b==5;
+
+4.
+ Use Collection int a [] = {2,5,7,15,1,6};
+   int a [] = {2,5,7,15,1,6};
+   output
+   13   < Max number minus second min num and result is 13
+     */
+}
