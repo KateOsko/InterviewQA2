99b18f9b1ae50af3998eaaa099896d5c47ae3fd1
Shahin <shahingadirli@gmail.com>
2022-04-28 11:49:18 -0400

    task 11

--- src/Question_11.java
+++ src/Question_11.java
@@ -1,3 +1,15 @@
 public class Question_11 {
+    public static void main(String[] args) {
 
+
+        int[] a = {9, 6, 7};
+
+        int min = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (min>a[i]) {
+                min = a[i];
+            }
+        }
+        System.out.println(min);
+    }
 }
